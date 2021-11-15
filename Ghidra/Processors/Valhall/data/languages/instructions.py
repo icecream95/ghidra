2c97343d50bc20d72811182aa84b6b1ea487b235
Icecream95 <ixn@disroot.org>
2021-11-15 14:07:07 +1300

    Switch to using Mako, add 32-bit source swizzles

---
+++ Ghidra/Processors/Valhall/data/languages/instructions.py
@@ -0,0 +1,221 @@
+from mako.template import Template
+from mako import exceptions
+
+src_ranges = (
+    ("r", 0, 5),
+    ("u", 0, 5),
+    ("i", 0, 4),
+    ("ts", 0, 2),
+    ("id", 0, 4),
+    ("t", 6, 7),
+)
+
+template = """
+define endian=little;
+define alignment=8;
+
+define space ram type=ram_space size=8 default;
+define space register type=register_space size=4;
+
+define token opword (64)
+% for src in range(1, 5):
+<% base = (src - 1) * 8 %>
+        src${src} = (${base},${base + 7})
+% for n, s, e in src_ranges:
+        src${src}${n} = (${base+s},${base+e})
+% endfor
+% endfor
+
+        absneg1      = (38,39)
+        absneg2      = (36,37)
+        absneg3      = (34,35)
+        absneg4      = (32,33)
+
+        widen1_32    = (36,39)
+        widen2_32    = (26,29)
+
+        D1           = (40,45)
+        DM           = (46,47)
+
+        op2      = (16,19)
+
+        clamp        = (32,33)
+
+        ofs          = (8,23) signed
+        constant     = (8,39)
+        sr_count     = (33,35)
+        load_lane    = (36,38)
+        unsigned_skip = (39,39)
+        op           = (48,56)
+        imm_mode     = (57,58)
+        action       = (59,61)
+        do_action    = (62,62)
+;
+
+define register offset=0x00 size=4 [
+% for reg in range(64):
+        r${reg}
+% endfor
+];
+
+define register offset=0x100 size=4 [
+% for reg in range(64):
+        u${reg}
+% endfor
+];
+
+# TODO: Use program_counter
+define register offset=0x200 size=8 [
+        PC SP
+];
+
+define register offset=0x300 size=4 [
+        tls_ptr tls_ptr_hi wls_ptr wls_ptr_hi
+        lane_id core_id program_counter
+];
+
+attach variables [ src1r src2r src3r src4r D1 ] [
+% for reg in range(64):
+        r${reg}
+% endfor
+];
+
+attach variables [ src1u src2u src3u src4u ] [
+% for reg in range(64):
+        u${reg}
+% endfor
+];
+
+attach values [ src1i src2i src3i src4i ] [
+        0x0 0xffffffff 0x7fffffff 0xfafcfdfe 0x1000000 0x80002000
+        0x70605030 0xc0b0a090 0x3020100 0x7060504 0xb0a0908 0xf0e0d0c
+        0x13121110 0x17161514 0x1b1a1918 0x1f1e1d1c 0x3f800000
+        0x3dcccccd 0x3ea2f983 0x3f317218 0x40490fdb 0x0 0x477fff00
+        0x5c005bf8 0x2e660000 0x34000000 0x38000000 0x3c000000
+        0x40000000 0x44000000 0x48000000 0x42480000
+];
+
+attach variables [ src1ts src2ts src3ts src4ts ] [
+        _ _ tls_ptr tls_ptr_hi _ _ wls_ptr wls_ptr_hi
+];
+
+attach variables [ src1id src2id src3id src4id ] [
+        _ _ lane_id _ _ _ core_id _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
+        _ _ _ _ _ program_counter _
+];
+
+
+% for s in range(1, 5):
+
+S${s}: src${s}r is src${s}r & src${s}t=0 { export src${s}r; }
+S${s}: "`"^src${s}r is src${s}r & src${s}t=1 { export src${s}r; }
+S${s}: src${s}u is src${s}u & src${s}t=2 { export src${s}u; }
+S${s}: src${s}i is src${s}i & src${s}t=3 & imm_mode=0 { export src${s}i; }
+S${s}: src${s}ts is src${s}ts & src${s}t=3 & imm_mode=${s} { export src${s}ts; }
+S${s}: src${s}id is src${s}id & src${s}t=3 & imm_mode=3 { export src${s}id; }
+
+% if s < 3:
+
+SW${s}_32: S${s} is S${s} & widen${s}_32<2 { export S${s}; }
+SW${s}_32: S${s}^".h0" is S${s} & widen${s}_32=2 { temp:4 = zext(S${s}[0,16]); export temp; }
+SW${s}_32: S${s}^".h1" is S${s} & widen${s}_32=3 { temp:4 = zext(S${s}[16,16]); export temp; }
+SW${s}_32: S${s}^".b0" is S${s} & widen${s}_32=4 { temp:4 = zext(S${s}[0,8]); export temp; }
+SW${s}_32: S${s}^".b1" is S${s} & widen${s}_32=5 { temp:4 = zext(S${s}[8,8]); export temp; }
+SW${s}_32: S${s}^".b2" is S${s} & widen${s}_32=6 { temp:4 = zext(S${s}[16,8]); export temp; }
+SW${s}_32: S${s}^".b3" is S${s} & widen${s}_32=7 { temp:4 = zext(S${s}[24,8]); export temp; }
+
+% endif
+% endfor
+
+#AN$: S$ is S$ & absneg$=0 { export S$; }
+#AN$: S$^".neg" is S$ & absneg$=1 { t:4 = 0:4 f- S$; export t; }
+#AN$: S$^".abs" is S$ & absneg$=2 { t:4 = abs(S$); export t; }
+#AN$: S$^".neg.abs" is S$ & absneg$=3 { t:4 = 0:4 f- abs(S$); export t; }
+
+AN1: S1 is S1 & absneg1=0 { export S1; }
+AN1: S1^".neg" is S1 & absneg1=1 { t:4 = 0:4 f- S1; export t; }
+AN1: S1^".abs" is S1 & absneg1=2 { t:4 = abs(S1); export t; }
+AN1: S1^".neg.abs" is S1 & absneg1=3 { t:4 = 0:4 f- abs(S1); export t; }
+
+AN2: S2 is S2 & absneg2=0 { export S2; }
+AN2: S2^".neg" is S2 & absneg2=1 { t:4 = 0:4 f- S2; export t; }
+AN2: S2^".abs" is S2 & absneg2=2 { t:4 = abs(S2); export t; }
+AN2: S2^".neg.abs" is S2 & absneg2=3 { t:4 = 0:4 f- abs(S2); export t; }
+
+# TODO: Fix these
+AN1.h: S1 is S1 & absneg1=0 { export S1; }
+AN1.h: S1^".neg" is S1 & absneg1=1 { t:4 = 0:4 f- S1; export t; }
+AN1.h: S1^".abs" is S1 & absneg1=2 { t:4 = abs(S1); export t; }
+AN1.h: S1^".neg.abs" is S1 & absneg1=3 { t:4 = 0:4 f- abs(S1); export t; }
+
+AN2.h: S2 is S2 & absneg2=0 { export S2; }
+AN2.h: S2^".neg" is S2 & absneg2=1 { t:4 = 0:4 f- S2; export t; }
+AN2.h: S2^".abs" is S2 & absneg2=2 { t:4 = abs(S2); export t; }
+AN2.h: S2^".neg.abs" is S2 & absneg2=3 { t:4 = 0:4 f- abs(S2); export t; }
+
+macro Store(reg, mask, val) {
+      maskv = (mask & 1) * 0xffff + (mask & 2) * 0x7fff8000;
+      reg = (val & maskv) | (reg & ~maskv);
+}
+
+macro StoreClamp(reg, mask, clamp, val) {
+      # if bit 1 is set, clamp to -1
+      clamp_m1 = zext(val f> 0xbf800000:4) * val + zext(val f<= 0xbf800000) * 0xbf800000:4;
+      val = zext((clamp & 1:1) != 0:1) * clamp_m1 + zext((clamp & 1:1) == 0:1) * val;
+
+      # if bit 2 is set, clamp to 0
+      clamp_0 = zext(val f> 0:4) * val + zext(val f<= 0) * 0:4;
+      val = zext((clamp & 2:1) != 0:1) * clamp_0 + zext((clamp & 2:1) == 0:1) * val;
+
+      # if bit 3 is set, clamp to 1
+      clamp_1 = zext(val f> 0x3f800000:4) * 0x3f800000:4 + zext(val f<= 0x3f800000:4) * val;
+      val = zext((clamp & 4:1) != 0:1) * clamp_1 + zext((clamp & 4:1) == 0:1) * val;
+
+      Store(reg, mask, val);
+}
+
+DC: "" is clamp=0 { export 0:1; }
+DC: "clamp_0_inf" is clamp=1 { export 2:1; }
+DC: "clamp_m1_1" is clamp=2 { export 5:1; }
+DC: "clamp_0_1" is clamp=3 { export 6:1; }
+
+define pcodeop clz;
+
+:MOV.i32 D1, S1 is op=0x91 & op2=0x0 & D1 & DM & S1 {
+        Store(D1, DM, S1);
+}
+:CLZ.i32 D1, S1 is op=0x91 & op2=0x4 & D1 & DM & S1 {
+        Store(D1, DM, clz(S1));
+}
+:IABS.s32 D1, S1 is op=0x91 & op2=0x8 & D1 & DM & S1 {
+        Store(D1, DM, zext(S1 s> 0) * S1 + zext(S1 s<= 0) * -S1);
+}
+:NOT.i32 D1, S1 is op=0x91 & op2=0xE & D1 & DM & S1 {
+        Store(D1, DM, ~S1);
+}
+
+# TODO: Saturate
+% for num, op, code in ((0, "IADD", "local temp = {} + {};"), (1, "ISUB", "local temp = {} - {};")):
+:${op}.u32 D1, SW1_32, SW2_32 is op=0xA0 & op2=${num} & D1 & DM & SW1_32 & SW2_32 {
+        ${code.format("SW1_32", "SW2_32")}
+        Store(D1, DM, temp);
+}
+% endfor
+
+:FADD.f32 D1 DC, AN1, AN2 is op=0xA4 & D1 & DC & DM & AN1 & AN2 {
+        StoreClamp(D1, DM, DC, AN1 f+ AN2);
+}
+:FADD.v2f16 D1 DC, AN1.h, AN2.h is op=0xA5 & D1 & DC & DM & AN1.h & AN2.h {
+        res:4 = 0;
+        res[0,16] = AN1.h[0,16] f+ AN2.h[0,16];
+        res[16,16] = AN1.h[16,16] f+ AN2.h[16,16];
+        StoreClamp(D1, DM, DC, res);
+}
+
+
+"""
+
+try:
+    print(Template(template).render(src_ranges=src_ranges))
+except:
+    print(exceptions.text_error_template().render())
